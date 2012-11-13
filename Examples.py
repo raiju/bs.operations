3fae7af449e1a7317c854ef4fba63e0f42e815ab
Yohan Jarosz <yohanjarosz@yahoo.fr>
2012-11-13 10:23:58 +0100

    remove extra directories and now you can put multiples plugins on the same file

---
+++ Examples.py
@@ -0,0 +1,72 @@
+from bs.operations.base import OperationPlugin  # import the base class to build your plugin
+import os
+import random
+
+
+# some shared information about these exemples
+meta = {'version': "1.0.1",
+        'author': "Yohan Jarosz",
+        'contact': "webmaster-bbcf@epfl.ch"}
+
+
+class Simple(OperationPlugin):
+
+    info = {
+        'title': 'SIMPLE',                             # The title of your operation
+        'description': 'A minimal example',          # Describe the operation's goal
+        'path': ['Tests', 'Examples', 'Simple'],    # Under which category the operation will be set
+        # Must be unique across all plugins
+        # First in the list mean higher category
+        'in': [{'id': 'input', 'type': 'text', 'required': True}],    # All input parameters
+        'out': [{'id': 'output', 'type': 'file'}],                     # All output parameters
+        'meta': meta,                                # Meta information (authors, version, ...)
+    }
+
+    def __call__(self, *args, **kw):
+        text = kw.get('input', '')                    # get the parameter back
+
+        path = self.temporary_path()                  # get a temporary path
+        with open(path, 'w') as f:                    # open a file & write the input
+            f.write(text)
+        self.new_file(path, 'output')                 # add a file to the result
+
+        return 1
+
+
+class ReadFile(OperationPlugin):
+
+    info = {
+        'title': 'ReadFile',
+        'description': "Read the file to find the number of characters and write the result in an output file. "
+                        "If randomize is checked, the result is between 0 and the file's number of characters ",
+        'path': ['Tests', 'Examples', 'Read a file'],
+        'in': [{'id': 'fname', 'label': 'File Name', 'type': 'text'},
+                {'id': 'input', 'label': 'File input', 'type': 'file', 'required': True},
+                {'id': 'randomize', 'label': 'Randomize', 'type': 'boolean'}
+        ],
+        'out': [{'id': 'output', 'type': 'file'}],
+        'meta': meta,
+        }
+
+    def __call__(self, *args, **kw):
+        # get parameters
+        fin = kw.get('input')
+        fname = kw.get('fname', '')
+        if fname == '':
+            fname = os.path.split(fin)[1]
+        randomize = kw.get('randomize', False)
+        # read the input file
+        nchar = 0
+        with open(fin, 'r') as rin:
+            for line in rin:
+                nchar += len(line)
+                # randomize if checked
+        if randomize:
+            nchar = random.randint(0, nchar - 1)
+            # take a temporary path where to write the file
+        fout = self.temporary_path(fname)
+        # write the result
+        with open(fout, 'w') as wout:
+            wout.write(str(nchar))
+            # add the file to the result
+        self.new_file(fout, 'output')
